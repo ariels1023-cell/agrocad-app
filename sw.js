f736e4b2a1621367f29535f1380c8a3fa219af06
ariels1023-cell <ariels.1023@gmail.com>
2026-08-17 15:50:40 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'agrocad-v1';
+const urlsToCache = [
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js',
+  'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/js/controls/OrbitControls.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve lo que está en caché (offline) si lo encuentra, si no, busca en internet
+        return response || fetch(event.request);
+      })
+  );
+});
